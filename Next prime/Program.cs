a17565b562a218ba858216deddaf1053ad71d890
merlijnbrinks <merlijnbrinks@gmail.com>
2020-06-28 22:27:59 +0200

    Added next prime

---
+++ Next prime/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Next_prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NextPrime(402));
+    
+        }
+
+        public static int NextPrime(int num) 
+        {
+            bool isPrime = false;
+            bool failed = false;
+            while(isPrime == false){
+                failed =false;
+                for(int i = 2;i<num;i++){
+                    
+                    decimal notPrime = Convert.ToDecimal(num)/Convert.ToDecimal(i);
+                    if ((notPrime % 1) == 0){
+                        failed = true;
+                        i = num;
+                    }
+
+                    
+                }
+                if(failed != true){
+                    isPrime = true;
+                }
+                if (isPrime==false){
+                    num++;
+                }
+
+
+            }
+            return num;
+        }
+    }
+}
