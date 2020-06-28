cf1c5130b455a9492bb78bcd081890b0d19b5c9b
merlijnbrinks <merlijnbrinks@gmail.com>
2020-06-28 21:53:00 +0200

    Adding first project which reverses the binary number

---
+++ Reverse binary string/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Reverse_binary_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = reversedBinaryInteger(5);
+            Console.WriteLine(test);
+        }
+
+        public static int reversedBinaryInteger(int num) 
+        {
+			string binNum = Convert.ToString(num, 2);
+            Console.WriteLine(binNum);
+            string reversedNum = "";
+			for (int i = binNum.Length; i>0; i-- ){
+                reversedNum = reversedNum + binNum[i-1].ToString();
+            }
+
+            Console.WriteLine(reversedNum);
+
+            return Convert.ToInt32(reversedNum, 2);
+        }
+    }
+}
