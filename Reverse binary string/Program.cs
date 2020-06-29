eefa227a62235b16fdb896f24cdd0474279ce2b2
Merlijn <44466724+Cookien00b@users.noreply.github.com>
2020-06-29 17:31:38 +0200

    Added comments

--- Reverse binary string/Program.cs
+++ Reverse binary string/Program.cs
@@ -6,21 +6,23 @@ namespace Reverse_binary_string
     {
         static void Main(string[] args)
         {
-            int test = reversedBinaryInteger(5);
-            Console.WriteLine(test);
+            Console.WriteLine(reversedBinaryInteger(5));
         }
 
         public static int reversedBinaryInteger(int num) 
         {
+		//converts the number to its binary equal in string form
 			string binNum = Convert.ToString(num, 2);
+		//write the binary number to the console
             Console.WriteLine(binNum);
             string reversedNum = "";
+		//Keeps adding the first, second... etc to the end of the string to reverse the string
 			for (int i = binNum.Length; i>0; i-- ){
                 reversedNum = reversedNum + binNum[i-1].ToString();
             }
-
+		//writes the inverted binary number to the console
             Console.WriteLine(reversedNum);
-
+		//Converts the binary string to the number it represents and returns it
             return Convert.ToInt32(reversedNum, 2);
         }
     }
