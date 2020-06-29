9dec5586341f84a0bcc55c1030142c7ccce2f1e2
Merlijn <44466724+Cookien00b@users.noreply.github.com>
2020-06-29 17:27:58 +0200

    Added comments

--- Next prime number/Program.cs
+++ Next prime number/Program.cs
@@ -6,19 +6,24 @@ namespace Next_prime
     {
         static void Main(string[] args)
         {
+            //Sends a number to check if its a prime and writes the output to the console
             Console.WriteLine(NextPrime(402));
     
         }
 
         public static int NextPrime(int num) 
         {
+            
             bool isPrime = false;
             bool failed = false;
+            //Keeps checking until the next prime number is found
             while(isPrime == false){
                 failed =false;
+                //checks if current number is a prime
                 for(int i = 2;i<num;i++){
                     
                     decimal notPrime = Convert.ToDecimal(num)/Convert.ToDecimal(i);
+                    //if the number devides from something else then 1 and itself it will stop the for loop
                     if ((notPrime % 1) == 0){
                         failed = true;
                         i = num;
@@ -26,9 +31,11 @@ namespace Next_prime
 
                     
                 }
+                //if it is a prime number it returns true and leaves the while statement
                 if(failed != true){
                     isPrime = true;
                 }
+                //if it isn't a prime number it increases it by one and checks again
                 if (isPrime==false){
                     num++;
                 }
