d67dff444f46b9e224ea8b67d8b11e093474ebab
merlijnbrinks <merlijnbrinks@gmail.com>
2020-07-01 11:59:48 +0200

    Added reverse and not

---
+++ Reverse and not/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_and_not
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseAndNot(12345));
+        }
+        public static string ReverseAndNot(int i) {
+        return string.Join(string.Empty, i.ToString().ToArray().Reverse())+i.ToString();	
+		}
+    }
+}
