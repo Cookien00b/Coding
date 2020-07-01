321a1d983245f85dea6b10d78326fa20a4fd23ba
merlijnbrinks <merlijnbrinks@gmail.com>
2020-07-01 12:20:30 +0200

    Added Factorials

---
+++ Factorials/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Factorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(factorial(3));
+        }
+
+        public static int factorial(int num) {
+            int[] factoNum = new int[num];
+            //get all numbers to multiply to get factorial
+            for(int i = 0; i<=num-1; i++){
+                factoNum[i] = num-i;
+            }
+            int factorialNum = 1;
+            //multiplies said numbers
+            foreach (int value in factoNum)
+            {
+                factorialNum *= value;
+            }
+            //returns the final awnser
+            return factorialNum;
+	    }
+    }
+}
