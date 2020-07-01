e3baa0ea581d13c6fe6e1d21a2ba829d90edead1
merlijnbrinks <merlijnbrinks@gmail.com>
2020-07-01 13:13:36 +0200

    added find the missing number

---
+++ Find the missing number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Find_the_missing_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MissingNum(new int[] { 1, 2, 3, 4, 6, 7, 8, 9, 10 }));
+        }
+        public static int MissingNum(int[] arr)
+        {
+            int total = 0;
+            foreach(int x in arr){
+                total += x;
+            }
+            return 55-total;
+        }
+    }
+}
