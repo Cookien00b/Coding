4833e37673e65aba75d2af42610df5d58e781c16
merlijnbrinks <merlijnbrinks@gmail.com>
2020-07-01 12:58:10 +0200

    Added XO counter

---
+++ XO Check/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace XO_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(XO("xooxx"));
+        }
+
+        public static bool XO(string str) 
+        {
+            int x = 0;
+            int o = 0;
+            foreach(char X in str){
+                if(X=='X'||X=='x') x++;
+                if(X=='O'||X=='o') o++;
+            }
+            if(x==o)return true;
+            
+            return false;
+      
+        }
+    }
+}
